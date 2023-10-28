dd9151764b6f9ad6f7b5a7a317ffee083613d6f0
Ryan Chung <ryanccj@gmail.com>
2023-10-29 04:00:09 +0800

    update main script

--- run.sh
+++ run.sh
@@ -13,58 +13,72 @@ DEL_META=false
 
 # ===========================
 
-echo "Step0. data checking"
+echo "Step1. data checking"
 cd pipeline/data_checking
+# >>>
 sh run.sh ${READ} ${REG} ${TAR}
+# >>>
 cd ..
 
 # --------------------------
 
-echo "Step1. chira"
+echo "Step2. chira"
 cd chira
 READ=${READ%.*}.fa
 REG=${REG%.*}.fa
 TAR=${TAR%.*}.fa
-# run.sh [fold_name] [regulator] [target] [thread] [seed_length(12)] [gap_penalty(6)] [mismatch_penalty(4)] [score_cutoff(18)]
+# run.sh [fold_name] [regulator] [target] [thread(8)] [seed_length(12)] [gap_penalty(6)] [mismatch_penalty(4)] [score_cutoff(18)]
+# >>>
 sh run.sh ${DATA} ${READ} ${REG} ${TAR} 8 12 6 4 18
+# >>>
 cd ..
 OUTPUT=chira/${DATA}_extract_dir/${DATA}.csv
 
 # --------------------------
 
-echo "Step2. find deletion"
+echo "Step3. find deletion"
 cd find_deletion
 REG=${REG%.*}.csv
 TAR=${TAR%.*}.csv
+# >>>
 sh run.sh ../${OUTPUT} ${TAR}
+# >>>
 cd ..
 OUTPUT=find_deletion/ALL_output/${DATA}_step1.csv
 
 # --------------------------
 
-echo "Step3. predict site"
+echo "Step4. predict site"
 cd predict_site
 # [n/extend_length]
 EXTEND=n
-sh run_pir.sh ../${OUTPUT} ${REG} ${TAR} ${EXTEND}
+
+# pirScan
+# >>>
+sh run_pirScan.sh ../${OUTPUT} ${REG} ${TAR} ${EXTEND}
+# >>>
 OUTPUT=predict_site/scan_output/${DATA}_step1_scan.csv
-sh run_mir.sh ../${OUTPUT} ${REG} ${TAR} ${EXTEND}
+
+# miRanda
+# >>>
+sh run_miRanda.sh ../${OUTPUT} ${REG} ${TAR} ${EXTEND}
+# >>>
 OUTPUT=predict_site/mir_output/${DATA}_step1_scan_mir.csv
-cd ..
 
-# --------------------------
-
-echo "Step4. RNAup"
-cd RNAup
-sh run.sh ../${OUTPUT} ${REG} ${TAR} ${EXTEND}
-OUTPUT=RNAup/RNAup_output/${DATA}_step1_scan_mir_RNAup.csv
+# RNAup
+# >>>
+sh run_RNAup.sh ../${OUTPUT} ${REG} ${TAR} ${EXTEND}
+# >>>
+OUTPUT=predict_site/up_output/${DATA}_step1_scan_mir_RNAup.csv
 cd ..
 
 # --------------------------
 
 echo "Step5. data processing"
 cd data_processing
+# >>>
 sh run.sh ../${OUTPUT} ${TAR}
+# >>>
 OUTPUT=data_processing/after_preprocess/${DATA}_step1_scan_mir_RNAup_final.csv
 cd ..
 
@@ -76,7 +90,9 @@ cd induce_22g
 EXTEND=25
 # [region/site/up/abu]
 TYPE=$5
+# >>>
 sh run.sh ../${OUTPUT} ${REG} ${TAR} ${EXTEND} ${TYPE}
+# >>>
 if [ $TYPE = "abu" ]
 then
     OUTPUT=induce_22g/add_abu_info/abu_${EXTEND}_${DATA}_step1_scan_mir_RNAup_final.csv
@@ -89,18 +105,30 @@ cd ..
 
 echo "Step7. generate figure"
 cd generate_figure
-# [pirScan/miRnada/RNAup]
+# [pirScan/miRanda/RNAup]
 TOOL=$4
 # normalization factor
 FACTOR=811.03
+# >>>
 sh run.sh ${DATA} ../${OUTPUT} ${TOOL} ${TYPE} ${FACTOR}
+# >>>
 cd ../../
 
 # --------------------------
 
 echo "Step8. collect files"
-cp pipeline/${OUTPUT} data/output/
-cp -r pipeline/generate_figure/figure data/output/
+DIR=${DATA}_$(date +%Y-%m-%d_%H-%M-%S)
+mkdir data/output/${DIR}
+cp pipeline/${OUTPUT} data/output/${DIR}/${DATA}.csv
+cp -r pipeline/generate_figure/figure data/output/${DIR}/
+cp -r pipeline/generate_figure/log data/output/${DIR}/
+cmd_log=data/output/${DIR}/log/command.log
+touch ${cmd_log}
+echo Read File: $1 >> ${cmd_log}
+echo Regulator File: $2 >> ${cmd_log}
+echo Transcript File: $3 >> ${cmd_log}
+echp Algorithm: $4 >> ${cmd_log}
+echo Enrichment Analysis Type: $5 >> ${cmd_log}
 
 if [ $DEL_META = true ]
 then
@@ -108,7 +136,7 @@ then
     rm pipeline/find_deletion/ALL_output/${DATA}*
     rm pipeline/predict_site/scan_output/${DATA}*
     rm pipeline/predict_site/mir_output/${DATA}*
-    rm pipeline/RNAup/RNAup_output/${DATA}*
+    rm pipeline/predict_site/up_output/${DATA}*
     rm pipeline/data_processing/after_preprocess/${DATA}*
     rm pipeline/${OUTPUT}
 fi
