3c2bdf4a4548f0f04c22d17c4c787ef856cff753
Ryan Chung <ryanccj@gmail.com>
2024-01-25 01:18:18 +0800

    update chira script

--- pipeline/chira/run.sh
+++ pipeline/chira/run.sh
@@ -6,7 +6,7 @@ f0=$2
 f1=$3
 # target
 f2=$4
-#
+# single or chimeras
 hybrid=$5
 
 mkdir ${F}_map_dir
@@ -26,11 +26,11 @@ chira/chira_quantify.py -b ${F}_merge_dir/segments.bed -m ${F}_merge_dir/merged.
 echo extract
 chira/chira_extract.py -l ${F}_quantify_dir/loci.counts -o ${F}_extract_dir/ -f1 ${f1} -f2 ${f2}
 
-if [ $hybrid = 'single']; then
+if [ $hybrid = 'single' ]; then
     echo singletons to csv
     python chira/chira_singleprocess.py -i ${f0} -t ${f2} -s ${F}_extract_dir/singletons -o ${F}_extract_dir/${F}_single.csv
 
-elif [ $hybrid = 'chimeras']; then
+elif [ $hybrid = 'chimeras' ]; then
     echo chimeras to csv
     python chira/chira_chimeprocess.py -i1 ${f0} -i2 ${f1} -c ${F}_extract_dir/chimeras -o ${F}_extract_dir/${F}_chimeras.csv
 else
