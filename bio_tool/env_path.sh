6ca1e4a54d32af8065bfacd782fdf55005b62a66
Ryan Chung <ryanccj@gmail.com>
2024-04-12 15:59:19 +0800

    add bioinformatics software

---
+++ bio_tool/env_path.sh
@@ -0,0 +1,14 @@
+# suite
+export PATH=$(pwd)/bowtie-1.2.3-linux-x86_64:$PATH
+export PATH=$(pwd)/bowtie2-2.4.1-linux-x86_64:$PATH
+export PATH=$(pwd)/ViennaRNA/bin:$PATH
+export PATH=$(pwd)/FastQC:$PATH
+export PATH=$(pwd)/TrimGalore-0.6.5:$PATH
+export PATH=$(pwd)/miRanda-3.3a/bin:$PATH
+export PATH=$(pwd)/bwa-0.7.5a:$PATH
+
+# hyb
+export PATH=$PATH:$(pwd)/clash_analyst/hyb-master/bin
+
+# CLAN
+export PATH=$PATH:$(pwd)/clash_analyst/CLAN_release/bin
